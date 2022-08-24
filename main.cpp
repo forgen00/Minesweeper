f725b5d1387404409f3de35df49eec558ab752f7
forgen <4Rgen1326@mail.ru>
2022-08-24 14:58:19 +0500

    123

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 IMAGE* img[12];
 int number = 0;
+const int WIDTH = 800, HEIGHT = 600;
 
 void read_images();
 void free_images();
@@ -16,15 +17,27 @@ void read_images() {
 }
 
 void free_images() {
-    for (int i = 0; i < 6; ++i) {
+    for (int i = 0; i < 12; ++i) {
         freeimage(img[i]);
     }
 }
 
 int main() {
-    initwindow(800, 600, "Minesweeper", 0, 0); // режим 2-страниц
-    // видима страница 0, активна страница 1
+    srand(time(0));
+    initwindow(WIDTH, HEIGHT, "Minesweeper", 0, 0, true);
+    read_images();
+   
+    int w = 32;
+    int gridLogic[12][12];
+    int gridView[12][12];
     int t = 0; // номер кадра (время)
+
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 10; j++) {
+            gridView[i][j] = 10;
+        }
+    }
+
     while (1)
     {
         setbkcolor(BLACK); // цвет фона
@@ -32,12 +45,19 @@ int main() {
         ++t;
         // рисование изображения
         
+        for (int i = 1; i <= 10; ++i) {
+            for (int j = 1; j <= 10; ++j) {
+                putimage(i * w, j * w, img[number], COPY_PUT);
+            }
+        }
 
 
 
-       
+        swapbuffers();
         delay(30); // задержка на 1/30 секунды
         if (kbhit()) break; // если нажата клавиша - завершить работу
     }
+    freeimage(img);
     closegraph();
+    return 0;
 }
